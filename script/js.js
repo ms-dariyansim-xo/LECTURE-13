f8805aa3df25656f638979423c776aa07b9db9ab
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-01 16:27:05 -0700

    last topic added in- color picker

--- script/js.js
+++ script/js.js
@@ -1,1 +1,8 @@
-alert("Alert tester");
+const cpBg = document.getElementById("cpBg");
+const body = document.getElementsByTagName("Body")[0];
+
+
+
+cpBg.addEventListener('change', function() {
+    body.style.backgroundColor = cpBg.nodeValue;
+})
