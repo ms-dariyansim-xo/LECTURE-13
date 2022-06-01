2f5eb00c8aa4b46234a065b3c0555e24c53d4f01
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-01 16:29:49 -0700

    Update js.js

--- script/js.js
+++ script/js.js
@@ -1,8 +1,8 @@
 const cpBg = document.getElementById("cpBg");
-const body = document.getElementsByTagName("Body")[0];
+const body = document.getElementsByTagName("body")[0];
 
 
 
 cpBg.addEventListener('change', function() {
-    body.style.backgroundColor = cpBg.nodeValue;
+    body.style.backgroundColor = cpBg.Value;
 })
